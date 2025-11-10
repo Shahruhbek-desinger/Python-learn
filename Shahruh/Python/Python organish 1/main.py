2ce4d2b116b3d36dd915f94a90c4c88a34f7a74d
Shahruhbek-desinger <duschanovabushahruhbek@gmail.com>
2025-11-10 14:12:24 +0500

    Add files via upload

--- Shahruh/Python/Python organish 1/main.py
+++ Shahruh/Python/Python organish 1/main.py
@@ -492,3 +492,118 @@
 # print("hello" in text)
 # print("z" not in text)
 
+# a = str("Hello World")
+# print(a.isalpha())
+
+# yosh = input("Yoshinngizni yozing: ")
+# ism = input("Isminggizni yozing: ")
+# sonmi = yosh.isalnum()
+# ism2 = str(ism).strip()
+# print(ism2)
+# harf = ism2.isalpha()
+
+# mal = dict(name=b , age=a)
+
+# if harf == True and sonmi == True:
+#     print(f"Sizning yoshinggiz {yosh} va isminggiz {ism.title()}")
+# else:
+#     print("Isminggizni va Yoshinggizni togri yozing")
+
+
+a = 5
+b = 4
+
+# & -- and
+# if a & b: 
+#     print("true")
+
+# | -- or
+# if a | b: 
+#     print("true")
+
+# ^ -- XOR
+# if a ^ b: 
+#     print("true")
+
+# listlar -------------------------------------------------------------------------------------------
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(1, "orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+# ------------------------------------------------------------------------------------------------------
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist]
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if "a" in x] # ichida a harfi borlarni oladi
+
+# print(newlist)
